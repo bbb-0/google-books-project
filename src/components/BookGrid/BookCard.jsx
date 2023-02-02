9e44b626a4ff493aa5b2d4c458e7bbaa6986ba1d
bbb <mabracode@gmail.com>
2023-02-03 09:35:16 +1100

    fix booklist/card | styling

--- src/components/BookGrid/BookCard.jsx
+++ src/components/BookGrid/BookCard.jsx
@@ -1,73 +1,27 @@
 import BookList from '../../container/BookList';
 import styles from './BookCard.module.scss';
 
-const BookCard = () => {
+const BookCard = ({ bookData }) => {
 	return (
-		<div className={styles.BookList}>
-			<h2>{bookData.volumeInfo.title}</h2>
-			<h3>{bookData.volumeInfo.authors}</h3>
+		<div className={styles.card}>
+			<h2 className={styles.container__title}>{bookData.volumeInfo.title}</h2>
+			<h3 className={styles.container__author}>
+				{bookData.volumeInfo.authors}
+			</h3>
 			{bookData.volumeInfo.imageLinks && (
 				<img
 					src={bookData.volumeInfo.imageLinks.thumbnail}
 					alt={bookData.volumeInfo.title}
 				/>
 			)}
+			<h3 className={styles.container__description}>
+				{bookData.volumeInfo.description}
+			</h3>
 		</div>
 	);
 };
 
 export default BookCard;
 
-// import { useEffect, useState } from 'react';
-// import Modal from '../Modal/Modal';
-// import styles from './BookCard.module.scss';
-
-// const Books = () => {
-// 	const [books, setBooks] = useState([]);
-// 	const [search, setSearch] = useState('');
-
-// 	useEffect(() => {
-// 		if (search === '') {
-// 			return;
-// 		}
-// 		async function getBooks() {
-// 			const response = await fetch(
-// 				`https://www.googleapis.com/books/v1/volumes?q=${search}`
-// 			);
-// 			const data = await response.json();
-// 			setBooks(data.items);
-// 		}
-// 		getBooks();
-// 	}, [search]);
-
-// 	return (
-// 		<div className={styles.books}>
-// 			<input
-// 				className={styles.books__search}
-// 				placeholder="Search for a book"
-// 				type="text"
-// 				onChange={(e) => setSearch(e.target.value)}
-// 			/>
-// 			<button onClick={() => setSearch('')}>Clear</button>
-
-// 			{books.map((book) => (
-// 				<div key={book.id} className={styles.bookCard}>
-// 					<h1 className={styles.title}>{book.volumeInfo.title}</h1>
-// 					<h2>{book.volumeInfo.authors}</h2>
-// 					{book.volumeInfo.imageLinks && (
-// 						<img
-// 							src={book.volumeInfo.imageLinks.thumbnail}
-// 							alt={book.volumeInfo.title}
-// 						/>
-// 					)}
-// 					{/* {books.map((book) => (
-// 						return <BookList key={book.id} bookData={book} />;
-// 					)} */}
-// 				</div>
-// 			))}
-// 			{/* <Modal show={show} item={bookItem} onClose={() => setShow(false)} /> */}
-// 		</div>
-// 	);
-// };
-
-// export default Books;
+// Need to use a place holder image when none availble ifrom the API.
+// Give user feedback when no description available (adding some checks)
--- src/container/BookList.jsx
+++ src/container/BookList.jsx
@@ -1,22 +1,39 @@
 import { useEffect, useState } from 'react';
+import BookCard from '../components/BookGrid/BookCard';
 import styles from './BookList.module.scss';
 
 const BookList = () => {
 	const [books, setBooks] = useState([]);
 	const [search, setSearch] = useState('');
 
+	// Why do we use useEffect hook? -> To trigger effect at very specific times in the lifecycle of our React application
+
+	// We do not want the fetch() to happen everytime the user inputs a new char.
+	// So we need to keep track of the input seperatly from the search term
+	// We need an extra state variable to keep track of the input (it will have an onChange function that is triggered every time the user clicks on a char)
+	// We will add a search button. When that button is called (onClick={handleSearch})
+	// handleSearch - update search to be the latest input + trigger API call + reset input to empty string
+
+	// Abstract the API call in its own function (put it in a service folder and import where needed)
+
+	const getBooks = async () => {
+		const response = await fetch(
+			`https://www.googleapis.com/books/v1/volumes?q=${search}`
+		);
+		const data = await response.json();
+		return data.items;
+	};
+
+	const handleSearch = async () => {
+		const fetchedBooks = await getBooks();
+		setBooks(fetchedBooks);
+	};
+
 	useEffect(() => {
 		if (search === '') {
 			return;
 		}
-		async function getBooks() {
-			const response = await fetch(
-				`https://www.googleapis.com/books/v1/volumes?q=${search}`
-			);
-			const data = await response.json();
-			setBooks(data.items);
-		}
-		getBooks();
+		handleSearch();
 	}, [search]);
 
 	return (
@@ -32,31 +49,11 @@ const BookList = () => {
 			</button>
 			<div className={styles.container}>
 				{books.map((book) => (
-					<div key={book.id} className={styles.books__card}>
-						<h1 className={styles.container__title}>{book.volumeInfo.title}</h1>
-						<h2 className={styles.container__author}>
-							{book.volumeInfo.authors}
-						</h2>
-						{book.volumeInfo.imageLinks && (
-							<img
-								className={styles.image}
-								src={book.volumeInfo.imageLinks.thumbnail}
-								alt={book.volumeInfo.title}
-							/>
-						)}
-					</div>
+					<BookCard key={book.id} bookData={book} />
 				))}
 			</div>
 		</div>
 	);
 };
 
-{
-	/* <Modal show={show} item={bookItem} onClose={() => setShow(false)} /> */
-}
-{
-	/* {books.map((book) => (
-				return <BookList key={book.id} bookData={book} />;
-			)} */
-}
 export default BookList;
